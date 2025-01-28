49488f3fcd4fd39f2a3550ba3c3c99b2eb6bd7c2
Natsu <134845191+IsnanJabar@users.noreply.github.com>
2025-01-28 14:30:47 +0700

    config.js

--- config.js
+++ config.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
+global.owner = ['6283863243187']
+global.ownernomer = "6283863243187"
+global.premium = ['6283165275486']
 global.packname = 'Sticker By'
 global.author = 'Naze Dev'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
